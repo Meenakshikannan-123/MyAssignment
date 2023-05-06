71343b901a49c92fdb1fb31184f7225e0c4eb13e
Meenakshikannan-123 <132699770+Meenakshikannan-123@users.noreply.github.com>
2023-05-07 00:00:15 +0530

    Add files via upload
    week 1

---
+++ week1day1/Car.java
@@ -0,0 +1,64 @@
+package week1day1;
+
+public class Car {
+	public void unlockTheCar() {
+		System.out.println("open the car");
+	}
+	public void openTheDoor() {
+		System.out.println("open the door");
+	}
+	public void closeTheDoor() {
+		System.out.println("close the door");
+	}
+			public void putOnSeatBelt() {
+				System.out.println("put on the seat belt");
+			}
+			public void startTheCar() {
+				System.out.println("start the car");
+			}
+			public void releaseTheHandbreak() {
+				System.out.println("release the handbreak");
+			}
+			public void applyClutch () {
+				System.out.println("apply clutch");
+			}
+			public void applyGear() {
+				System.out.println("apply gear");
+			}
+			public void startDriving() {
+				System.out.println("start driving");
+			}
+			public void applyBreak() {
+				System.out.println("apply break");
+			}
+			public void applyAccelerate() {
+				System.out.println("apply accelerate");
+			}
+			public void switchOnAc() {
+				System.out.println("switch on Ac ");
+			}
+			public void useHorn() {
+				System.out.println("use horn");
+			}
+			public void takeReverse() {
+				System.out.println("take reverse");
+			}
+			public static void main(String[] args) {
+				Car car =new Car();
+				car.unlockTheCar();
+				car.openTheDoor();
+				car.closeTheDoor();
+				car.putOnSeatBelt();
+				car.startTheCar();
+				car.releaseTheHandbreak();
+				car.applyClutch();
+				car.applyGear();
+				car.startDriving();
+				car.applyBreak();
+				car.applyAccelerate();
+				car.switchOnAc();
+				car.useHorn();
+				car.takeReverse();
+			}
+		
+}
---
+++ week1day1/Mobile.java
@@ -0,0 +1,58 @@
+package week1day1;
+
+public class Mobile {
+	public void sendMessage() {
+		System.out.println("sendMessage");
+		
+	}
+	public void alarm() {
+		System.out.println("alarm set");
+		
+	}
+	public void callToSomeone() {
+		System.out.println("call to someone");
+		
+	}
+	public void movies() {
+		System.out.println("To see movies");
+		
+	}
+	public void playGames() {
+		System.out.println("play games");
+		
+	}
+	public void takePhotos() {
+		System.out.println("takephotos");
+		
+	}
+	public void browse() {
+		System.out.println("browse");
+		
+	}
+	public void useWhatsapp() {
+		System.out.println("use whatsapp");
+		
+	}
+	public void useSocialMedia() {
+		System.out.println("use social media");
+		
+	}
+	public void useCalculator () {
+		System.out.println("use calculator");
+		
+	}
+	public static void main(String[] args) {
+		Mobile mobile =new Mobile();
+		mobile.sendMessage();
+		mobile.alarm();
+		mobile.callToSomeone();
+		mobile.movies();
+		mobile.playGames();
+		mobile.takePhotos();
+		mobile.browse();
+		mobile.useWhatsapp();
+		mobile.useSocialMedia();
+		mobile.useCalculator();
+		
+	}
+}
